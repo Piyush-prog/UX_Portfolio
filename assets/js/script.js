9e6ce80aff0595fe8ddfb857adf994c19d04b6e3
Piyush Goyal <piyushg944@gmail.com>
2024-11-11 23:33:08 -0500

    Header, Footer, Intro

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Function to load an HTML file into a specific element
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+// Load the header and footer
+loadHTML("header-placeholder", "../../components/header.html");
+loadHTML("footer-placeholder", "../../components/footer.html");
