931b65a18280d6c1ac9010ad447777f23848f3f2
Piyush Goyal <60353847+Piyush-prog@users.noreply.github.com>
2024-11-30 22:23:16 -0500

    Content, About Me etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ function loadHTML(elementId, filePath) {
 }
 
 // Load the header and footer
-loadHTML("header-placeholder", "../../components/header.html");
-loadHTML("footer-placeholder", "../../components/footer.html");
-loadHTML("skills-placeholder", "../../components/skills.html");
-loadHTML("conscious_cravings", "../../components/project_cards/conscious_cravings.html");
-loadHTML("home_away_from_home", "../../components/project_cards/home_away_from_home.html");
-loadHTML("building_credit_confidence", "../../components/project_cards/building_credit_confidence.html");
-loadHTML("explorations_graphic_design", "../../components/project_cards/explorations_graphic_design.html");
+loadHTML("header-placeholder", "/components/header.html");
+loadHTML("footer-placeholder", "/components/footer.html");
+loadHTML("skills-placeholder", "/components/skills.html");
+loadHTML("conscious_cravings", "/components/project_cards/conscious_cravings.html");
+loadHTML("home_away_from_home", "/components/project_cards/home_away_from_home.html");
+loadHTML("building_credit_confidence", "/components/project_cards/building_credit_confidence.html");
+loadHTML("drinks_menu", "/components/project_cards/drinks_menu.html");
+loadHTML("cca", "/components/project_cards/cca.html");
+loadHTML("holiday_greeting", "/components/project_cards/holiday_greeting.html");
