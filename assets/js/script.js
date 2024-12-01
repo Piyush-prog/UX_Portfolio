343829258baf4dda4477cb5601a1ecbdf7449e0f
Piyush Goyal <60353847+Piyush-prog@users.noreply.github.com>
2024-12-01 13:00:34 -0500

    Test absolute paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,12 @@ function loadHTML(elementId, filePath) {
 }
 
 // Load the header and footer
-loadHTML("header-placeholder", "/components/header.html");
-loadHTML("footer-placeholder", "/components/footer.html");
-loadHTML("skills-placeholder", "/components/skills.html");
-loadHTML("conscious_cravings", "/components/project_cards/conscious_cravings.html");
-loadHTML("home_away_from_home", "/components/project_cards/home_away_from_home.html");
-loadHTML("building_credit_confidence", "/components/project_cards/building_credit_confidence.html");
-loadHTML("drinks_menu", "/components/project_cards/drinks_menu.html");
-loadHTML("cca", "/components/project_cards/cca.html");
-loadHTML("holiday_greeting", "/components/project_cards/holiday_greeting.html");
+loadHTML("header-placeholder", "/UX_Portfolio/components/header.html");
+loadHTML("footer-placeholder", "/UX_Portfolio/components/footer.html");
+loadHTML("skills-placeholder", "/UX_Portfolio/components/skills.html");
+loadHTML("conscious_cravings", "/UX_Portfolio/components/project_cards/conscious_cravings.html");
+loadHTML("home_away_from_home", "/UX_Portfolio/components/project_cards/home_away_from_home.html");
+loadHTML("building_credit_confidence", "/UX_Portfolio/components/project_cards/building_credit_confidence.html");
+loadHTML("drinks_menu", "/UX_Portfolio/components/project_cards/drinks_menu.html");
+loadHTML("cca", "/UX_Portfolio/components/project_cards/cca.html");
+loadHTML("holiday_greeting", "/UX_Portfolio/components/project_cards/holiday_greeting.html");
